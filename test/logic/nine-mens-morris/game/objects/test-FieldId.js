f8f5a244774c8d8f513d6822d97c20cf98176d1e
Patrick Mennig <patrick.mennig@fhws.de>
2017-03-23 16:37:37 +0100

    added test cases for field ids

---
+++ test/logic/nine-mens-morris/game/objects/test-FieldId.js
@@ -0,0 +1,29 @@
+const assert            = require('assert');
+const fieldIdFactory    = require('../../../../../src/logic/nine-mens-morris/game/objects/FieldId');
+const enumFieldIdErrors = require('../../../../../src/logic/nine-mens-morris/game/objects/errors/enumFieldIdErrors');
+
+
+describe('FieldId', function () {
+
+    describe('constructor', function () {
+
+        it('should create a field id', function () {
+            const fieldId = fieldIdFactory.createFieldId(0);
+            assert.equal(fieldId.id, 0);
+        });
+
+        it('should not create a field id, too low', function () {
+            assert.throws(() => fieldIdFactory.createFieldId(-33), TypeError, enumFieldIdErrors.INVALID_ID);
+        });
+
+        it('should create a field id, too high', function () {
+            assert.throws(() => fieldIdFactory.createFieldId(742), TypeError, enumFieldIdErrors.INVALID_ID);
+        });
+
+        it('should create a field id, no number', function () {
+            assert.throws(() => fieldIdFactory.createFieldId('0'), TypeError, enumFieldIdErrors.INVALID_ID);
+        });
+
+    });
+
+});
