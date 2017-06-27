2d1476198996e62ddf5fce9ba1076bd89a3d1888
Patrick Mennig <patrick.mennig@fhws.de>
2017-06-27 13:07:31 +0200

    closing issue #1, test cases added

--- src/logic/nine-mens-morris/rules/MillRules.js
+++ src/logic/nine-mens-morris/rules/MillRules.js
@@ -32,8 +32,8 @@ class MillRules {
         const mills = millCombinations[move.getToId()];
 
         const fromId = move.getFromId();
-        const isFromInMill0 = fromId ? mills[0].filter(p => p === fromId).length > 0 : false;
-        const isFromInMill1 = fromId ? mills[1].filter(p => p === fromId).length > 0 : false;
+        const isFromInMill0 = typeof fromId === 'number' ? mills[0].filter(p => p === fromId).length > 0 : false;
+        const isFromInMill1 = typeof fromId === 'number' ?  mills[1].filter(p => p === fromId).length > 0 : false;
 
         return !isFromInMill0 && isMill(move.getToken(), positionsForMill(mills[0], board))
             || !isFromInMill1 && isMill(move.getToken(), positionsForMill(mills[1], board));
